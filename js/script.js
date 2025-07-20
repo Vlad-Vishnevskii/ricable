c952eb7a92d71e4c36d0268a6d744889afec5154
Влад Вишневский <v.vishnevskii@idynsys.org>
2025-07-20 22:58:47 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -99,4 +99,26 @@ document.addEventListener('DOMContentLoaded', function () {
     } else {
     }
   });
+
+  const boxes = document.querySelectorAll('.box');
+
+  window.addEventListener('scroll', checkBoxes);
+  window.addEventListener('resize', checkBoxes);
+  checkBoxes();
+
+  function checkBoxes() {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    boxes.forEach((box, index) => {
+      const boxTop = box.getBoundingClientRect().top;
+
+      if (boxTop < triggerBottom) {
+        box.style.transitionDelay = `${index * 0.1}s`;
+        box.classList.add('show');
+      } else {
+        box.style.transitionDelay = `0s`;
+        box.classList.remove('show');
+      }
+    });
+  }
 });
